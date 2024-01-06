d6413fb9a4a10829aecabb8c27c862aa2aa22ce8
saurabh <saurabhu154@gmail.com>
2024-01-06 20:56:28 +0530

    added onclick for image change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Hero } from "./components/Hero";
+import { Playground } from "./components/Playground";
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Playground />
+
+          </>
+        }
+      />
+    </Routes>
+  </Suspense>
+  )
 }
 
-export default App;
+export default App
