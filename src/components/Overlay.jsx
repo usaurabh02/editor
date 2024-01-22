fc21061ec782865bd741208a31d373170cb0d025
saurabh <saurabhu154@gmail.com>
2024-01-22 19:42:32 +0530

    added button for meeting

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaVideo } from "react-icons/fa";
 
 export const Overlay = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    <div className='absolute top-3 left'>Overlay for video and image navigation</div>
-  )
-}
+    <div className="absolute top-3 left-3">
+      <button
+        onClick={handleClick}
+        className="bg-black flex gap-1 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded"
+      >
+        <div className="font-[20px] italic">
+        {isPlaying ? 'Online' : 'Meeting'}  </div>
+        <div className="mt-[2px]">
+          {isPlaying ? <FaVideo size={20} color="green" /> : <FaVideo size={20} />}
+        </div>
+      </button>
+    </div>
+  );
+};
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Saurabh from "../asssets/Bedroom2.png";
 import Siddharth from "../asssets/bedroom2H1.png";
 import Hotspot from "../asssets/icon.png";
 
 export const Playground = () => {
   const [bgImage, setBgImage] = useState(Saurabh);
-  const [showHotspot, setShowHotspot] = useState(true);
+  const [showHotspot, setShowHotspot] = useState(false);
+  const cordinates = ["5 -2 -4", "6 -2 -3", "2 -3 -6"]
 
   const handleClick = () => {
-    setBgImage(Siddharth);
     setShowHotspot(false);
+    setBgImage(Siddharth);
+    setTimeout(()=>{
+      setShowHotspot(true)
+    },2500)
   };
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setShowHotspot(true)
+    },2500)
+  },[])
+  console.log(cordinates)
 
   return (
     <div className="w-full h-screen bg-black">
       <a-scene>
         <a-entity vr-mode-ui="enabled: true" cursor="rayOrigin: mouse;">
           <a-sky id="360-image" src={bgImage} animation="easing: easeInOutSine">
-          {showHotspot && (
-            <a-image
-              src={Hotspot}
-              scale="1 1 1"
-              position="5 -2 -4"
-              rotation="90 120 0"
-              onClick={handleClick}
-            ></a-image>
-            )}
+          {showHotspot && 
+            Array.from(cordinates).map((i)=>
+                <a-image
+                src={Hotspot}
+                scale="1 1 1"
+                position={i}
+                rotation="90 120 0"
+                onClick={handleClick}
+              ></a-image>
+            
+            )
+            }
           </a-sky>
         </a-entity>
       </a-scene>
